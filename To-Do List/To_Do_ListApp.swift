f7ef2ca919c46ac5b21441d643c1a2696383e51f
popnotfound <aneeshleisure21@gmail.com>
2021-07-28 10:36:21 -0500

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Aneesh Pushparaj on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
